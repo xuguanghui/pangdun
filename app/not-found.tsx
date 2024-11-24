cb11938746cbc783cc07f3b0eb3de0589ab447d3
xuguanghui <498816258@qq.com>
2024-11-24 21:54:16 +0800

    heihei

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,7 +10,7 @@ export default function notFound() {
       		<div className="text-5xl font-dark font-bold">404</div>
             <p
               className="text-2xl md:text-3xl font-light leading-normal"
-            >Sorry we couldn't find this page. </p>
+            >Sorry we couldnot find this page. </p>
           <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
           
           <Link href="/"  className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700">back to homepage</Link>
